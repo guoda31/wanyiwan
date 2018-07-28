24846f7b9cdbb90bb9e64208972d00a0adbde4dd
Guoda Wang <guoda31@hotmail.com>
2018-07-28 15:51:38 +0800

    can now pull up stock graph from GUI

--- GUI.py
+++ GUI.py
@@ -1,25 +1,34 @@
 import tkinter as tk
 import tkinter.messagebox
 from tkinter import Frame, Button, Label
-from tkinter import RIGHT, BOTH, LEFT, TOP, X, Y
+from tkinter import RIGHT, BOTH, LEFT, TOP, BOTTOM, X, Y
+from main import getSingleStockData
 
 
 #Window Contents
 mainwindow = tk.Tk()
 mainwindow.title = ("Single Stock Data Display")
 # width * height + distance from left + d from right
-mainwindow.geometry('200x300+100+100') 
+mainwindow.geometry('800x500+100+100') 
 
-Lframe = Frame(mainwindow)
+#Left panel, used for controls
+Lframe = Frame(mainwindow, bg="green")
 leftLabel = Label(Lframe, text="this is the left frame")
+leftLabel.pack(side =TOP)
+Lframe.pack(side='left', fill=BOTH) 
 
-leftLabel.pack(side =LEFT)
-Rframe = Frame(mainwindow)
+#Right panel, used for displays
+Rframe = Frame(mainwindow, bg="blue")
 Rightlabel = Label(Rframe, text="this is the right frame")
-Rightlabel.pack(side=RIGHT)
-Lframe.pack(side='left')    
+Rightlabel.pack(side=TOP)
 Rframe.pack(side=RIGHT, padx = 10, pady = 10)
 
+#listbox in right panel
+data = 0
+lb = tk.Listbox(Rframe, listvariable = data)
+lb.pack(side=RIGHT)
+
+#FUN BUTTON YAY
 clicked = False
 def hitButton():
     global clicked
@@ -30,7 +39,25 @@ def hitButton():
         print(tk.messagebox.showerror(title="???", message="no more lols"))   
 
 b = Button(Lframe, text="hit for some lols", command=hitButton)
-b.pack(side='bottom')
+
+def fetch():
+    global data
+    name = stocknameTB.get()
+    print (name)
+    data = getSingleStockData(name)
+    print (data)
+
+
+#get stocks
+#textbox for entering stock name
+stocknameTB = tk.Entry(Lframe)
+
+#confirm button
+getdata = Button(Lframe, text="get data", command=fetch)
+
+stocknameTB.pack(side=BOTTOM)
+b.pack(side=TOP)
+getdata.pack(side=BOTTOM)   #why is button above textbox??
 #Run
 mainwindow.mainloop()
 
--- main.py
+++ main.py
@@ -10,10 +10,27 @@ from alpha_vantage.timeseries import TimeSeries
 #Plotting Lib.
 import matplotlib.pyplot as plt
 
+def getSingleStockData(stk_symbl):
+    if stk_symbl == "":
+        print ("no symbol")
+        return -1
+    else:
+        print ("fetching %s" % stk_symbl)
+        ts = TimeSeries(key='3QGQAO7J85VT4771', output_format='pandas')
+        data, meta_data = ts.get_intraday(symbol=stk_symbl,interval='1min', outputsize='full')
+        data['4. close'].plot()
+        plt.title('Intraday Times Series for the ' +stk_symbl+ ' stock (1 min)')
+        plt.show()
+        #print(data)
+        return data
+"""
 #Main Program
 while (True):
     print ("Enter the Stock Symbol (Type quit to exit the program):")
-    stk_symbl = input()
+    ss=input()
+    getSingleStockData(ss)
+    """
+"""     stk_symbl = input()
     if stk_symbl == "quit": #exiting command
         break
     else:
@@ -23,5 +40,6 @@ while (True):
         plt.title('Intraday Times Series for the ' +stk_symbl+ ' stock (1 min)')
         plt.show()
 
-    print(data)
+    print(data) 
+"""
 
